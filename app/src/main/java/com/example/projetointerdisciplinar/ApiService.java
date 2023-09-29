3c3f1dbb6872628d67a5dbb487599e857ff02aa2
vinicius1diniz <vinicius.diniz@germinare.org.br>
2023-09-29 10:11:59 -0300

    Subindo integração da tela perfil

--- app/src/main/java/com/example/projetointerdisciplinar/ApiService.java
+++ app/src/main/java/com/example/projetointerdisciplinar/ApiService.java
@@ -4,5 +4,8 @@ import retrofit2.http.GET;
 import retrofit2.http.Query;
 public interface ApiService {
     @GET("api/user/verifyaccount")
-    Call<Boolean> getAllUsers(@Query("email") String email, @Query("senha") String senha);
+    Call<Boolean> verifyAccount(@Query("email") String email, @Query("senha") String senha);
+
+    @GET("api/user/getByEmail")
+    Call<Usuario> getUserById(@Query("email") String email);
 }
--- app/src/main/java/com/example/projetointerdisciplinar/Perfil.java
+++ app/src/main/java/com/example/projetointerdisciplinar/Perfil.java
@@ -2,13 +2,63 @@ package com.example.projetointerdisciplinar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Perfil extends AppCompatActivity {
+    private static final String BASE_URL = "https://api-interdisciplinar.onrender.com";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd");
+        TextView nome = findViewById(R.id.textView8);
+        TextView usuario = findViewById(R.id.textView10);
+        TextView campoEmail = findViewById(R.id.textView11);
+        TextView senha = findViewById(R.id.textView14);
+        TextView data_nasc = findViewById(R.id.textView16);
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        ApiService apiService = retrofit.create(ApiService.class);
+
+        Intent intent = getIntent();
+        String email = intent.getStringExtra("email");
+        Call<Usuario> call = apiService.getUserById(email);
+        call.enqueue(new Callback<Usuario>() {
+            @Override
+            public void onResponse(Call<Usuario> call, Response<Usuario> response) {
+                if (response.isSuccessful()) {
+                    Usuario apiResponse = response.body();
+                    nome.setText(apiResponse.getNome_real());
+                    usuario.setText(apiResponse.getNome_usuario());
+                    campoEmail.setText(apiResponse.getEmail());
+                    senha.setText(apiResponse.getSenha());
+                    data_nasc.setText(formatter.format(apiResponse.getData_nascimento()));
+                } else {
+                    Log.e("NetworkError", "Erro na chamada de rede2: " + response.code());
+                }
+            }
+            @Override
+            public void onFailure(Call<Usuario> call, Throwable t) {
+                Log.e("NetworkError", "Erro na chamada de rede: " + t.getMessage());
+            }
+        });
     }
 }
