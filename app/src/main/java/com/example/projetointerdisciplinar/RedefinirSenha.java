ac4462e365e2c1e806d92e5f22899f664d0eec7a
ArthurBurgarinFrank <arthur.burgarin@gmail.com>
2023-09-29 20:00:00 -0300

    subir alteração

---
+++ app/src/main/java/com/example/projetointerdisciplinar/RedefinirSenha.java
@@ -0,0 +1,14 @@
+package com.example.projetointerdisciplinar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RedefinirSenha extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_redefinir_senha);
+    }
+}
