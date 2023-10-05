5bd740efbc5751dc8b7f612e939715c299c3471d
ArthurBurgarinFrank <arthur.burgarin@gmail.com>
2023-10-05 15:36:25 -0300

    Commit mudanças perfil

--- app/src/main/java/com/example/projetointerdisciplinar/EscolherFoto.java
+++ app/src/main/java/com/example/projetointerdisciplinar/EscolherFoto.java
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -17,7 +19,8 @@ import java.io.IOException;
 
 public class EscolherFoto extends AppCompatActivity {
     ImageView verImagem;
-
+    private int rotationAngle = 90;
+    private int rotationAngle2 = 270;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +30,7 @@ public class EscolherFoto extends AppCompatActivity {
         verImagem = findViewById(R.id.verImg);
         ImageButton camera = findViewById(R.id.imgCamera);
         ImageButton galeria = findViewById(R.id.imgGaleria);
+
         camera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -34,18 +38,23 @@ public class EscolherFoto extends AppCompatActivity {
                 resultadoIntent.launch(intent);
             }
         });
+
         galeria.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI );
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                 resultadoIntentGallery.launch(intent);
             }
         });
     }
+
     ActivityResultLauncher<Intent> resultadoIntent = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
         if (result.getResultCode() == RESULT_OK) {
             Bundle extras = result.getData().getExtras();
             Bitmap imagemBitmap = (Bitmap) extras.get("data");
+
+            imagemBitmap = rotateBitmap(imagemBitmap, rotationAngle);
+
             verImagem.setImageBitmap(imagemBitmap);
         }
     });
@@ -55,7 +64,10 @@ public class EscolherFoto extends AppCompatActivity {
             Bundle extras = result.getData().getExtras();
             Uri localImgSelecionada = result.getData().getData();
             try {
-                Bitmap imagemBitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),localImgSelecionada);
+                Bitmap imagemBitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), localImgSelecionada);
+
+                imagemBitmap = rotateBitmap2(imagemBitmap, rotationAngle2);
+
                 verImagem.setImageBitmap(imagemBitmap);
             } catch (IOException e) {
                 throw new RuntimeException(e);
@@ -69,4 +81,16 @@ public class EscolherFoto extends AppCompatActivity {
         startActivity(intent);
         finish();
     }
+
+    private Bitmap rotateBitmap(Bitmap source, float angle) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
+    }
+    private Bitmap rotateBitmap2(Bitmap source, float angle) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
+    }
+
 }
